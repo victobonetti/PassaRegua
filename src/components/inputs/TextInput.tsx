797b9fdd8592e6e3cadfa07d872e0ba73a68af79
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-07-02 15:22:53 -0300

    table search on

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -1,8 +1,19 @@
-export default function TextInput({ set, value, name, id, style, label, err }: { set: (value: React.SetStateAction<string>) => void, value?: string, name: string, id: string, style?: string, label?: string, err?: string }) {
+interface inputProps {
+    set: (value: React.SetStateAction<string> ) => void,
+    value?: string,
+    name: string,
+    id: string,
+    style?: string,
+    placeholder?: string,
+    label?: string,
+    err?: string
+}
+
+export default function TextInput({ set, value, name, id, style, label, placeholder, err }: inputProps) {
     return (
         <>
             <label className="text-xs font-semibold text-slate-400" htmlFor={id}>{label}</label>
-            <input value={value} autoComplete="none" onChange={e => set(e.target.value)} className={`${style} mb-2 shadow appearance-none border rounded w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none focus:shadow-outline`} type="text" name={name} id={id} />
+            <input placeholder={placeholder} value={value} autoComplete="none" onChange={e => set(e.target.value)} className={`${style} mb-2 shadow appearance-none border rounded w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none focus:shadow-outline`} type="text" name={name} id={id} />
             <span className=" mb-2 text-xs text-red-500">{err}</span>
         </>
     )
--- src/components/table/tableComponent.tsx
+++ src/components/table/tableComponent.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
+import TextInput from "../inputs/TextInput";
+import './table.css'
+import React from 'react';
+
 
 interface tableComponentProps<T extends { created_at: string, id: string }> {
     data: T[],
@@ -10,56 +14,85 @@ interface tableComponentProps<T extends { created_at: string, id: string }> {
 }
 
 export default function TableComponent<T extends { created_at: string, id: string }>({ data, dataKeys, header, deleteMethod, otherMethods, otherMethodsText }: tableComponentProps<T>) {
-
     const [optionsActive, setOptionsActive] = useState('');
+    const [search, setSearch] = useState('');
+    const [filteredData, setFilteredData] = useState(data);
 
-    const manageOptions = (id: string) => {
-        optionsActive == id ? setOptionsActive('') : setOptionsActive(id)
+    const manageOptions = (id?: string) => {
+        id ? (optionsActive === id ? setOptionsActive('') : setOptionsActive(id)) : (optionsActive !== '' ? setOptionsActive('') : null);
     }
 
+    const handleSearch = () => {
+        if (search.trim().length > 1) {
+            const searchData = search.trim().toLowerCase();
+            const filtered = data.filter((item: T) =>
+                Object.values(item).some((search) =>
+                    String(search).toLowerCase().includes(searchData)
+                )
+            );
+            setFilteredData(filtered);
+
+        } else {
+            setFilteredData(data);
+        }
+    };
+
+    useEffect(() => {
+        handleSearch()
+    }, [search])
+
     return (
-        <table className="w-full">
-            <thead className="select-none bg-slate-400 font-semibold flex w-full items-center text-sm p-1">
-                <tr className=" w-full flex items-center">
-                    {header?.map((t, i) => <td key={i} className=" text-xs  w-full text-slate-600">{t.toUpperCase()}</td>)}
-                </tr>
-            </thead>
-            <tbody className="text-slate-300 w-full flex flex-col">
-                {data.map((d: T, i) => {
-                    return (
-                        <tr key={d.id} className={` ${ optionsActive == d.id ? "text-emerald-100 bg-emerald-900 border" : "bg-slate-800 odd:bg-slate-700"} p-1 w-full flex items-center `}>
-                            {
-                                dataKeys?.map((key) => {
-                                    return (
-                                        <td key={i} className={` w-full text-xs  whitespace-nowrap`}>
-                                            {String(d[key as keyof T])}
+        <>
+            <TextInput label="Buscar..." placeholder="Digite algo..." value={search} set={setSearch} name="search" id="search" />
+            <table onClick={() => manageOptions()} className="w-full">
+                <thead className="select-none bg-slate-400 font-semibold flex w-full items-center text-sm p-1">
+                    <tr className="w-full flex items-center">
+                        {header?.map((t, i) => <td key={i} className="text-xs w-full text-slate-600">{t.toUpperCase()}</td>)}
+                    </tr>
+                </thead>
+                <tbody className="text-slate-300 w-full flex flex-col">
+                    {filteredData.map((d: T, i) => (
+                        <div key={d.id}>
+                            <tr className={`bg-slate-800 odd:bg-slate-700 p-1 w-full flex items-center`}>
+                                {dataKeys?.map((key) => (
+                                    <td key={key} className={`w-full text-xs whitespace-nowrap`}>
+                                        {String(d[key as keyof T])}
+                                    </td>
+                                ))}
+                                <td className="w-full whitespace-nowrap">
+                                    <button onClick={() => manageOptions(d.id)} className={`${optionsActive !== d.id ? "bg-slate-400 text-slate-100" : "bg-slate-100 text-slate-600"} rounded w-12 text-xs hover:opacity-95 h-4 font-black`}>
+                                        Editar
+                                    </button>
+                                </td>
+                            </tr>
+                            {optionsActive === d.id && (
+                                <>
+                                    <tr className="w-full flex self-end z-10 text-xs text-slate-500">
+                                        {dataKeys?.map(() => (
+                                            <td className="w-full"></td>
+                                        ))}
+                                        <td className="flex w-full">
+                                            <div className="rounded-b w-20 appear bg-slate-200 self-start flex flex-col justify-start items-start py-3 pl-1 pr-4">
+                                                {deleteMethod && (
+                                                    <button className="text-start text-red-400 cursor-pointer hover:opacity-50 border-b w-full border-slate-300" onClick={() => deleteMethod(d)}>
+                                                        Excluir
+                                                    </button>
+                                                )}
+                                                {otherMethods && otherMethodsText && otherMethods.length === otherMethodsText.length && otherMethods.map((m, i) => (
+                                                    <button className="text-start border-b w-full border-slate-300 cursor-pointer hover:opacity-50" onClick={() => m(d)} key={i}>
+                                                        {otherMethodsText[i]}
+                                                    </button>
+                                                ))}
+                                            </div>
                                         </td>
-                                    )
-                                })
-                            }
-                            <td className=" w-full flex whitespace-nowrap">
-                                <button onClick={() => manageOptions(d.id)} className={`${ optionsActive != d.id ? " bg-slate-400 text-slate-100 w-12 rounded" : " bg-slate-100 text-slate-600 w-16 rounded-l"} pl-1.5 text-start text-xs hover:opacity-95 h-4  font-black`}>Editar</button>
-
-                                {
-                                    optionsActive == d.id &&
-                                    <div className=" text-xs ml-16 flex rounded-r rounded-bl flex-col absolute bg-slate-100 text-slate-600">
-
-                                        <div className=" py-3 pl-1 pr-4 flex flex-col justify-start items-start">
-                                            {deleteMethod &&
-                                                <button className="text-red-400 cursor-pointer hover:opacity-50" onClick={() => deleteMethod(d)}>Excluir</button>
-                                            }
-
-                                            {otherMethods && otherMethodsText && otherMethods.length === otherMethodsText.length && otherMethods.map((m, i) => {
-                                                return <button className=" cursor-pointer hover:opacity-50" onClick={() => m(d)}>{otherMethodsText[i]}</button>
-                                            })}
-                                        </div>
-                                    </div>
-                                }
-                            </td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                                    </tr>
+                                    <tr></tr>
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
